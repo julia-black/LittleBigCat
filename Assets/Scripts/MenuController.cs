d2c47aeaad19eb3630df12089245e57ddbe8afe0
Julia Black <b-r-cat@ya.ru>
2019-03-04 01:11:48 +0400

    Added Main menu screen

---
+++ Assets/Scripts/MenuController.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MenuController : MonoBehaviour
+{
+
+    public void ToMainMenu()
+    {
+        SceneManager.LoadScene(1);
+    }
+
+    public void LoadScene(string sceneName)
+    {
+        SceneManager.LoadScene(sceneName);
+    }
+  
+}
